b76681eeb4635370d298df1ea0f2b01e4507626a
Aden Mariyappa <46941201+AdenMar@users.noreply.github.com>
2022-11-07 13:28:55 -0500

    Message output reworked
    PC Name:
    Last User:
    Issue:
    Action:

--- patch_compliance_check.py
+++ patch_compliance_check.py
@@ -16,10 +16,6 @@ def main(path, saveDir):
     sh = wb["all"]
     outF = open(saveDir, "a")
 
-    tempOn = []
-    tempOnLen = 0
-    tempOff = []
-    tempOffLen = 0
     header = []
 
     for col in range(1,sh.max_column+1):
@@ -32,6 +28,7 @@ def main(path, saveDir):
     colStatus = header.index("Availability") + 1
 
     siteHolder = sh.cell(2,colSite).value
+    newMessage(siteHolder,outF)
 
     # itterate through rows (start at 2 avoid header)
     # index bySite [n][0] is on [n][1] is off
@@ -40,65 +37,34 @@ def main(path, saveDir):
         currLastUser = sh.cell(row,colLastUser).value
         currName = sh.cell(row,colName).value
         currStatus = sh.cell(row,colStatus).value
-        
+
         if (currSite != siteHolder):
-            writeAndLog("",outF)
-            message = "Good Morning!\n\nWe noticed that a couple of your systems appear to be "
-            header = "\tPC Name\t\tLast User"
-
-            # used for testing
-            # print(siteHolder)
-            # print(f"on: {tempOnLen}")
-            # print(f"off: {tempOffLen}")
-
-            if ((tempOnLen > 0)  & (tempOffLen > 0)):
-                message = message + "out of date or offline.\n"
-            elif ((tempOnLen > 0)  & (tempOffLen == 0)):
-                message = message + "out of date.\n"
-            elif ((tempOnLen > 0)  & (tempOffLen == 0)):
-                message = message + "offline.\n"
-            
-            writeAndLog(message,outF)
-
-            if (tempOnLen > 0):
-                writeAndLog(f"For your out of date systems, please let us know a good time to schedule a reboot:\n\n{header}",outF)
-
-                for i in tempOn:
-                    writeAndLog("\t" + i[0] + "\t\t" + i[1] ,outF)
-                writeAndLog("\nIt is important to keep these systems up to date to minimize security risk and ensure your computers run as effectively as possible.\n",outF)
-            
-            if (tempOffLen > 0):
-                writeAndLog(f"For your offline systems that are still in use, please boot them so we can ensure they are up to date:\n\n{header}",outF)
-
-                for i in tempOff:
-                    writeAndLog("\t" + i[0] + "\t\t" + i[1],outF)
-                writeAndLog("\nIf no longer in use, please let us know instead so we may act accordingly.\n",outF)
-             
-            writeAndLog("Thank you,\nAden and Kristen\n",outF)
-            
-            writeAndLog("___" + str(currSite) + "___",outF)
-
+            newMessage(currSite,outF)
             # return to defaults
-            tempOn = []
-            tempOnLen = 0
-            tempOff = []
-            tempOffLen = 0
             siteHolder = currSite
 
-        # add status to on or off holders
+        writeAndLog("Computer Name: " + str(currName),outF)
+        writeAndLog("Last Logged in User: " + str(currLastUser),outF)
+
         if (currStatus == "true"):
-            tempOn.append([currName,currLastUser])
-            tempOnLen += 1
+            writeAndLog("Issue: Your PC is out of date",outF)
+            writeAndLog("Action to be taken: Schedule a time with us for a reboot",outF)
         elif (currStatus == "false"):
-            tempOff.append([currName,currLastUser])
-            tempOffLen += 1
-            
+            writeAndLog("Issue: Your PC is offline",outF)
+            writeAndLog("Action to be taken: Power on the system or let us know if it is no longer in use",outF)
+
+        writeAndLog("",outF)
 
 def writeAndLog(content, file):
     file.write(content + "\n")
     # for debug perposes
     # print("File Updated")
     # print(content)
+
+def newMessage(site, file):
+    writeAndLog("_____" + str(site) + "_____\n",file)
+    writeAndLog("We proactively monitor your computers for missing Critical and Security Updates of the Operating System and have identified the following issue(s) that require attention.\n",file)
+    writeAndLog("Please review and let us know if you would like assistance with the action to be taken.\n\n",file)
 
 if __name__=="__main__":
     date = datetime.datetime.utcnow()
@@ -131,5 +97,3 @@ if __name__=="__main__":
     #         else:
     #             print("Failsafe: too many folders with same name convention")
     #             break
-            
-        
